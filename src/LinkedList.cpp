7bdaf2c13a6b5f260bf2039b30469634bd11ce7f
Danil Filippov <damarreve@list.ru>
2020-11-16 23:17:28 +0400

    Добавил реализацию методов, поправил демку

--- src/LinkedList.cpp
+++ src/LinkedList.cpp
@@ -2,47 +2,6 @@
 #include "LinkedList.h"
 
 /*
-Реализации методов структуры элемента списка.
- */
-
-template<typename T>
-Entry<T>::Entry()
-{
-    this->next = nullptr;
-}
-
-template<typename T>
-Entry<T>::Entry(T value)
-{
-    this->value = value;
-    this->next = nullptr;
-}
-
-template<typename T>
-T Entry<T>::get_value()
-{
-    return this->value;
-}
-
-template<typename T>
-void Entry<T>::set_value(T value)
-{
-    this->value = value;
-}
-
-template<typename T>
-Entry<T>* Entry<T>::get_next()
-{
-    return this->next;
-}
-
-template<typename T>
-void Entry<T>::set_next(Entry<T>* next)
-{
-    this->next = next;
-}
-
-/*
 Реализации методов класса связного списка.
  */
 
@@ -71,13 +30,13 @@ void LinkedList<T>::add(T value)
 {
     if (first_element == nullptr)
     {
-        first_element = new Entry<T>(value);
+        first_element = new Entry(value);
         current_element = first_element;
     }
     else
     {
-        current_element->set_next(new Entry<T>(value));
-        current_element = current_element->get_next();
+        current_element->next = new Entry(value);
+        current_element = current_element->next;
     }
 }
 
@@ -87,11 +46,11 @@ LinkedList<T>::~LinkedList()
     // Если пустой список, то и удалять нечего
     if (first_element == nullptr) return;
     // Если есть хотя бы один элемент - удалим по очереди все
-    Entry<T>* current;
-    Entry<T>* to_delete = first_element;
+    Entry* current;
+    Entry* to_delete = first_element;
     do
     {
-        current = to_delete->get_next();
+        current = to_delete->next;
         delete to_delete;
         to_delete = current;
     } while (current != nullptr);
@@ -100,28 +59,108 @@ LinkedList<T>::~LinkedList()
 template<typename T>
 void LinkedList<T>::print_list()
 {
-    Entry<T>* entry = first_element;
+    Entry* entry = first_element;
     std::cout << '[';
     do
     {
-        std::cout << entry->get_value() << (entry->get_next() == nullptr ? "" : ", ");
-        entry = entry->get_next();
+        std::cout << entry->value << (entry->next == nullptr ? "" : ", ");
+        entry = entry->next;
     } while (entry != nullptr);
     std::cout << ']' << std::endl;
+}
+
+template<typename T>
+int LinkedList<T>::size()
+{
+    if (first_element == nullptr) return 0;
+    Entry* pointer = first_element;
+    int size = 1;
+    while ((pointer = pointer->next) != nullptr) size++;
+    return size;
+}
+
+template<typename T>
+void LinkedList<T>::insert(T value, int index)
+{
+    if (index < 0 || index > size()) throw std::runtime_error("LinkedList::insert(T, int): Неверный индекс.");
+    Entry* previous = first_element;
+    if (index == 0)
+    {
+        // Меняем первый элемент
+        first_element = new Entry(value);
+        first_element->next = previous;
+    }
+    else if (index == size())
+    {
+        // Проверим - может быть индекс соответствует размеру списка и достаточно просто добавить элемент
+        add(value);
+    }
+    else
+    {
+        // Находим предыдущий элемент перед необходимым индексом
+        for (int i = 1; i < index; i++) previous = previous->next;
+        // Вставляем значение
+        Entry* new_entry = new Entry(value);
+        new_entry->next = previous->next;
+        previous->next = new_entry;
+    }
+}
+
+template<typename T>
+int LinkedList<T>::find(T value)
+{
+    int counter = 0;
+    Entry* pointer = first_element;
+    while (pointer != nullptr)
+    {
+        if (pointer->value == value) return counter;
+        pointer = pointer->next;
+        counter++;
+    }
+    return -1;
+}
+
+template<typename T>
+T LinkedList<T>::get_first()
+{
+    return first_element->value;
+}
+
+template<typename T>
+T LinkedList<T>::get(int index)
+{
+    int counter = 0;
+    Entry* pointer = first_element;
+    while (counter++ != index) pointer = pointer->next;
+    return pointer->value;
+}
+
+template<typename T>
+T LinkedList<T>::get_last()
+{
+    return current_element->value;
+}
+
+template<typename T>
+bool LinkedList<T>::is_empty()
+{
+    return size() == 0;
+}
+
+template<typename T>
+void LinkedList<T>::clear()
+{
+    this->~LinkedList();
+    // Отличие от деструктора в том, что нужно заново инициализировать переменные
+    first_element = nullptr;
+    current_element = first_element;
 }
 
 /*
 Возможные типы списка. Если нужны какие-то другие типы - можно добавить по аналогии.
 */
 
-template class Entry<int>;
 template class LinkedList<int>;
-
-template class Entry<float>;
 template class LinkedList<float>;
-
-template class Entry<double>;
 template class LinkedList<double>;
-
-template class Entry<std::string>;
 template class LinkedList<std::string>;
--- src/ListTest.cpp
+++ src/ListTest.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
+#include <cstdlib>
 #include "LinkedList.h"
 
 constexpr int ARRAY_SIZE = 5;
 
 int main()
 {
+    srand(static_cast<unsigned int>(time(0)));
     float* arr = new float[ARRAY_SIZE];
-    for (int i = 0; i < ARRAY_SIZE;) arr[i++] = static_cast<float>(rand() / static_cast<float>(RAND_MAX) * 10.0);
+    for (int i = 0; i < ARRAY_SIZE;) arr[i++] = static_cast<float>(rand() / static_cast<float>(RAND_MAX) * 1000.0);
     LinkedList<float>* list = new LinkedList<float>(arr, ARRAY_SIZE);
     list->print_list();
+    std::cout << "List size is " << list->size() << std::endl;
+    list->insert(10.0, 3);
+    list->print_list();
+    std::cout << "Value '10' on position #" << list->find(10) << std::endl;
+    std::cout << "First element == " << list->get_first() << std::endl;
+    std::cout << "Element #2 == " << list->get(2) << std::endl;
+    std::cout << "Element #3 == " << list->get(3) << std::endl;
+    std::cout << "Element #4 == " << list->get(4) << std::endl;
+    std::cout << "Last element == " << list->get_last() << std::endl;
+    std::cout << "List is " << (list->is_empty() ? "" : "not ") << "empty" << std::endl;
+    list->clear();
+    std::cout << "List is " << (list->is_empty() ? "" : "not ") << "empty" << std::endl;
     delete list;
     return 0;
 }
